6130418baea243f90982c81bf78c125704157f47
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2023-01-30 20:13:49 +0000

    added tamagochi project

---
+++ SEB3/week7A/app.js
@@ -0,0 +1,39 @@
+// Error Handlers 
+
+// function checkError(){
+//     throw new Error("This is an Error from me")
+// }
+
+// checkError();
+
+
+// Objects
+
+let myAddress ={
+    streetName: "London Bridge",
+    homeNumber: 12,
+    flat :{
+        number: 90,
+        floor: "second floor" 
+    }
+}
+
+console.log(myAddress.person)
+
+console.log("homeNumber" in myAddress)
+
+console.log(typeof myAddress["streetName"])
+
+
+// setIntervals
+
+// let paragraph = document.getElementById("paragraph");
+
+
+
+
+// setInterval(
+
+//     function show(){
+//         return paragraph.innerHTML += "Hi" // paragraph.innerHtml = paragraph.innerHTMl + hi
+//     }, 1000);
