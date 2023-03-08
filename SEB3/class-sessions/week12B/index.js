3dbe9d9fa159f89af0e7ca419599e0c045a9cb4b
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2023-03-08 18:20:22 +0000

    week 12B

---
+++ SEB3/class-sessions/week12B/index.js
@@ -0,0 +1,18 @@
+// sport , cloths , flowers, cars , etc  
+
+// database =  [{id, }, {} , {}] 
+
+// 2 get request  -  (/sports) - (/ sports/basketballs)  - (sports/ )
+
+// 2 get with id  -  /sport/:id - sport/basketball/:id
+
+
+// 2 post request - 
+
+// post with id - 
+
+
+// 2 delete with id  - 
+
+
+// 2 update with id - 
