01e46c65168c3710124e5b388f2e1c406aa85196
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2023-04-17 23:41:39 +0100

    added code for session 17B

---
+++ SEB3/class-sessions/week17A-19B/FE-React/shopping-app/src/components/Button.js
@@ -0,0 +1,11 @@
+
+function Button({handleClickMe}){
+    return(
+        <div>
+            <button onClick={handleClickMe}>Click Me</button>
+        </div>
+
+    )
+}
+
+export default Button;
--- SEB3/class-sessions/week17A-19B/FE-React/shopping-app/src/components/Footer.js
+++ SEB3/class-sessions/week17A-19B/FE-React/shopping-app/src/components/Footer.js
@@ -1,16 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import "../App.css"
 
-function Footer() {
+function Footer({Linkedin , Twitter ,Instagram }) {
   return (
     <>
-      <Navbar bg="light" variant="light">
+      <Navbar bg="light" variant="light" className='footer'>
         <Container>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Twitter</Nav.Link>
-            <Nav.Link href="#features">Linkedin</Nav.Link>
-            <Nav.Link href="#pricing">Instagram</Nav.Link>
+            <Nav.Link href="#home">{Linkedin}</Nav.Link>
+            <Nav.Link href="#features">{Twitter}</Nav.Link>
+            <Nav.Link href="#pricing">{Instagram}</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
